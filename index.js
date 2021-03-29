8898387e62c1556b91d6bdc7cf0810e9cee761cf
Cy Bunayog <cybunayog@gmail.com>
2021-03-29 10:03:21 -0700

    Added a prompt module

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 const fs = require('fs');
 const readline = require('readline');
 const prompt = require('prompt');
+const promptSync = require('prompt-sync')();
 const moment = require('moment');
 const { google } = require('googleapis');
 const promptSchema = require('./prompt');
 
 // Scope that the API will read and other predefined variables
 const SCOPES = ['https://www.googleapis.com/auth/calendar'];
-const CALENDAR_ID = 'primary'; // Primary is the default
+let calendarId;
 const CLIENT_SECRET = 'credentials.json';
 
 // Initial setup for readline package
@@ -26,15 +27,19 @@ const TOKEN_PATH = 'token.json';
 
 // Load client secrets from a local file.
 fs.readFile(CLIENT_SECRET, (err, content) => {
+
+  // Get the calendar ID first
+  calendarId = promptSync("What is the calendar's ID? (If personal, use 'primary'.)");
+
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Calendar API.
-  rl.question("Would you like to look at (list) or create an (event)?", answer => {
+  rl.question("Would you like to look at (list) or create an (event)? ", answer => {
     switch (answer) {
       case 'event':
-        console.log("You chose:", answer);
+        console.log("You chose: ", answer, calendarId);
         return authorize(JSON.parse(content), insertEvents);
       case 'list':
-        console.log("You chose:", answer);
+        console.log("You chose: ", answer, calendarId);
         return authorize(JSON.parse(content), listEvents);
       default:
         console.log('Closing prompt....');
@@ -42,6 +47,7 @@ fs.readFile(CLIENT_SECRET, (err, content) => {
     }
   });
 });
+
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -73,12 +79,12 @@ function getAccessToken(oAuth2Client, callback) {
     access_type: 'offline',
     scope: SCOPES,
   });
-  console.log('Authorize this app by visiting this url:', authUrl);
+  console.log('Authorize this app by visiting this url: ', authUrl);
 
   rl.question('Enter the code from that page here: ', (code) => {
     rl.close();
     oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
+      if (err) return console.error('Error retrieving access token ', err);
       oAuth2Client.setCredentials(token);
       // Store the token to disk for later program executions
       fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
@@ -94,10 +100,10 @@ function getAccessToken(oAuth2Client, callback) {
  * Lists the next 10 events on the user's primary calendar.
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function listEvents(auth) {
+function listEvents(auth, id) {
   const calendar = google.calendar({version: 'v3', auth});
   calendar.events.list({
-    calendarId: CALENDAR_ID,
+    calendarId,
     timeMin: (new Date()).toISOString(),
     maxResults: 10,
     singleEvents: true,
@@ -114,12 +120,11 @@ function listEvents(auth) {
 
       // Kill program
       process.exit(130);
-      }
+    }
     console.log('No upcoming events found.');
     return;
-    }
   });
-}
+  }
 
 /**
  * Creates an event on the user's primary calendar.
@@ -166,14 +171,14 @@ function insertEvents(auth) {
     // Add event to calendar
     calendar.events.insert({
       auth: auth,
-      calendarId: CALENDAR_ID,
+      calendarId,
       resource: event,
       }, (err, ev) => {
         if (err) {
           console.error('There was an error contacting the Calendar service: ', err);
           return;
         }
-        console.log('Success!! Event created: %s', ev.data.htmlLink);
+        console.log('Success!! Event created at %s', ev.data.htmlLink);
       
         // Kill program 
         process.exit(130);
